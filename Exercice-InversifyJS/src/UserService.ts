6b43360592d2fa7ddac0fd6ac7f4891bb41c1cf7
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-06-02 16:40:49 +0200

    Demo Theia

--- Exercice-InversifyJS/src/UserService.ts
+++ Exercice-InversifyJS/src/UserService.ts
@@ -4,7 +4,9 @@ import { User } from "./model";
 
 @injectable()
 export class UserService {
-  constructor(@inject(HttpClientInterface) private httpClient: HttpClientInterface) {}
+  @inject(HttpClientInterface) private httpClient!: HttpClientInterface;
+
+  // constructor(@inject(HttpClientInterface) private httpClient: HttpClientInterface) {}
   async fetchUsers() {
     return await this.httpClient.get<User[]>('http://localhost:3000/users');
   }
