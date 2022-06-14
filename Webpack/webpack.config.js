ac4482e9039c1198b120a2df00a7171205e483c7
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-06-14 13:01:35 +0200

    Webpack

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,20 +1,43 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: './src/index.html'
-  }),
-];
+function generateConfig(env, args) {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ];
 
-/** @type {import('webpack').Configuration} */
-const config = {
-  entry: './src/js/index.js',
-  output: {
-    clean: true,
-    filename: 'bundle.js'
-  },
-  devtool: false,
-  plugins,
-};
+  // devtool: false, // pour prod sans afficher les sources
+  // devtool: 'source-map', // pour prod avec les sources originales
+  // devtool: 'eval-source-map', // pour dev avec les sources originales
+  const devtool = args.mode === 'development' ? 'eval-source-map' : false;
 
-module.exports = config;
+  /** @type {import('webpack').Configuration} */
+  const config = {
+    entry: "./src/js/index.js",
+    output: {
+      clean: true,
+      filename: args.mode === 'development' ? 'bundle.js' : 'bundle.[contenthash].js',
+    },
+    devtool,
+    plugins,
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+    target: ["web", "es5"],
+  };
+
+  return config;
+}
+
+module.exports = generateConfig;
